2a9ee3b6eecfc1743973d31eed6f4934e2454ae9
Abhi Jain <devabhixda@gmail.com>
2023-04-22 23:19:34 +0530

    Init GRE word search

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import data from './assets/data.json';
+import values from './assets/values.json';
+import Select from 'react-select';
 
 function App() {
+  const [selectedOption, setSelectedOption] = useState('');
+  const [options, setOptions] = useState();
+ 
+  useEffect(()=>{
+    console.log(Object.keys(data).sort())
+    setOptions(Object.keys(data).sort().map(item=>({
+      label:item,
+      value:item
+    })))
+  },[])
+
+  const handleSearchChange = (selectedOption) => {
+    setSelectedOption(selectedOption)
+  }
+
+  const {value: itemData}=selectedOption
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 style={{textAlign: 'center'}}>GRE Word Search</h1>
+      <div style={{width: '40%', margin: 'auto'}}>
+        <Select
+          value={selectedOption}
+          onChange={handleSearchChange}
+          options={options}
+        />
+      </div>
+      <div key={itemData} style={{display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column'}}>
+      {
+        selectedOption == '' ? <h2>Please select a word</h2> : <div>
+          <div style={{textAlign: 'left'}}>
+            <h2>{itemData}</h2>
+            <h3>Meaning: {values[itemData]?.[0].meaning}</h3>
+            <h3>Sentence: {values[itemData]?.[0].sentence}</h3>
+          </div>
+          <img src={data[itemData]} alt={itemData} />
+        </div>
+      }
+      </div>
     </div>
   );
 }
